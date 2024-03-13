6481446821acd93f6645c92a725a7d211c0df2ee
Jackson Barreto <jackson.barreto@gmail.com>
2024-03-13 16:46:13 +0000

    feat: evaluation types

--- models/Kafka_Message.go
+++ models/Kafka_Message.go
@@ -9,3 +9,16 @@ type KafkaErrorMessage struct {
 type KafkaMessage struct {
 	Origin string `json:"origin"`
 }
+
+type EvaluationRequest struct {
+	InstitutionID string `json:"institution_id"`
+	URL           string `json:"url"`
+}
+
+type EvaluationResponse struct {
+	InstitutionID    string                 `json:"institution_id"`
+	Origin           string                 `json:"origin"`
+	StartTime        int64                  `json:"start_time"`
+	EndTime          int64                  `json:"end_time"`
+	EvaluationResult map[string]interface{} `json:"evaluation_result"`
+}
